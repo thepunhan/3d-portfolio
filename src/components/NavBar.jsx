a9b1b15807a59e06270853e5aaff41300d60da85
thepunhan <v68d74kq78@privaterelay.appleid.com>
2026-01-16 17:06:48 +0400

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,24 +1,17 @@
+// src/components/NavBar.jsx (YENİ HALİ)
 import { useState, useEffect } from "react";
-
-import { navLinks } from "../constants";
+import { navLinks } from "../constants"; // Constants'tan import ediyoruz
 
 const NavBar = () => {
-  // track if the user has scrolled down the page
   const [scrolled, setScrolled] = useState(false);
 
   useEffect(() => {
-    // create an event listener for when the user scrolls
     const handleScroll = () => {
-      // check if the user has scrolled down at least 10px
-      // if so, set the state to true
       const isScrolled = window.scrollY > 10;
       setScrolled(isScrolled);
     };
 
-    // add the event listener to the window
     window.addEventListener("scroll", handleScroll);
-
-    // cleanup the event listener when the component is unmounted
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
@@ -26,11 +19,13 @@ const NavBar = () => {
     <header className={`navbar ${scrolled ? "scrolled" : "not-scrolled"}`}>
       <div className="inner">
         <a href="#hero" className="logo">
+          {/* Logo metni için JSON'dan veri çekeceğiz */}
           Adrian JSM
         </a>
 
         <nav className="desktop">
           <ul>
+            {/* navLinks artık JSON'dan geliyor */}
             {navLinks.map(({ link, name }) => (
               <li key={name} className="group">
                 <a href={link}>
